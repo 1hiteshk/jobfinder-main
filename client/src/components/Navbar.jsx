47acdbb3758872d07e57daba2270a00af7a3adf8
1hiteshk <1hiteshk@gmail.com>
2024-07-22 01:07:52 +0530

    Remove node_modules

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -142,7 +142,7 @@ const Navbar = () => {
             {!user?.token ? (
               <Link to='/user-auth'>
                 <CustomButton
-                  title='Sign In'
+                  title='Sign in'
                   containerStyles='text-blue-600 py-1.5 px-5 focus:outline-none hover:bg-blue-700 hover:text-white rounded-full text-base border border-blue-600'
                 />
               </Link>
